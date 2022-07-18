bd1d6f2ad60dd4add0826c6464ecfe96c533aca5
matovu-farid <matovu90@gmail.com>
2022-07-19 01:36:02 +0300

    Add next and previous buttons

--- src/components/Slider/index.js
+++ src/components/Slider/index.js
@@ -2,8 +2,8 @@ import React, { useContext } from 'react';
 import { ImageContext } from '../../contexts/imageContext';
 import SliderImage from '../SliderImage';
 import SwipeDetector from '../SwipeDetector';
-// import { ImageContext } from '../../contexts/imageContext';
 import style from './style.module.scss';
+import { ImCircleLeft, ImCircleRight } from 'react-icons/im';
 
 const Slider = ({ images }) => {
   const { goToNextImage, goToPreviousImage } = useContext(ImageContext);
@@ -11,13 +11,15 @@ const Slider = ({ images }) => {
   return (
     <SwipeDetector onLeftSwipe={() => goToNextImage()} onRightSwipe={() => goToPreviousImage()}>
       <section className={style['slider-section']}>
-        
+        <ImCircleLeft className={style.button} onClick={goToPreviousImage} />
         <div className={style.slider}>
           {images.map((image, index) => (
             <SliderImage key={image.id} image={image} index={index} />
           ))}
 
         </div>
+        
+          <ImCircleRight className={style.button} onClick={goToNextImage} />
       </section>
 
     </SwipeDetector>
