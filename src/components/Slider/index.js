498335366c6df648b9f75c42880e1e75000fcbb5
matovu-farid <matovu90@gmail.com>
2022-07-19 03:12:21 +0300

    Remove unwanted files

--- src/components/Slider/index.js
+++ src/components/Slider/index.js
@@ -2,27 +2,24 @@ import React, { useContext } from 'react';
 import { ImCircleLeft, ImCircleRight } from 'react-icons/im';
 import { ImageContext } from '../../contexts/imageContext';
 import SliderImage from '../SliderImage';
-import SwipeDetector from '../SwipeDetector';
 import style from './style.module.scss';
 
 const Slider = ({ images }) => {
   const { goToNextImage, goToPreviousImage } = useContext(ImageContext);
 
   return (
-    <SwipeDetector onLeftSwipe={() => goToNextImage()} onRightSwipe={() => goToPreviousImage()}>
-      <section className={style['slider-section']}>
-        <ImCircleLeft className={style.button} onClick={goToPreviousImage} />
-        <div className={style.slider}>
-          {images.map((image, index) => (
-            <SliderImage key={image.id} image={image} index={index} />
-          ))}
+    <section className={style['slider-section']}>
+      <ImCircleLeft className={style.button} onClick={goToPreviousImage} />
+      <div className={style.slider}>
+        {images.map((image, index) => (
+          <SliderImage key={image.id} image={image} index={index} />
+        ))}
 
-        </div>
+      </div>
 
-        <ImCircleRight className={style.button} onClick={goToNextImage} />
-      </section>
+      <ImCircleRight className={style.button} onClick={goToNextImage} />
+    </section>
 
-    </SwipeDetector>
   );
 };
 export default Slider;
